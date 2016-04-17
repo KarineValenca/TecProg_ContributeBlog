5a3ca84754b0a80816e5919ae45c27d0babdf190
Attany <nathalyaraujoo@gmail.com>
2016-04-17 14:55:49 -0300

    Applying stylesheet in BlogOwner class

--- src/model/BlogOwner.java
+++ src/model/BlogOwner.java
@@ -1,33 +1,21 @@
 package model;
 
-
 import java.util.*;
 
 public class BlogOwner extends User {
 
 	private List<Blog> blogs;
-	
-	
-	
-	
+		
 	public List<Blog> getBlogs() {
 		return blogs;
 	}
-
-
-
 
 	public void setBlogs(List<Blog> blogs) {
 		this.blogs = blogs;
 	}
 
-
-
-
 	public BlogOwner() {
+		
 	}
-	
-	
-	
 
 }
