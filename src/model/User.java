4d308a75b35e667ddd4d494fe7662de54007f605
Karine Valença <valenca.karine@gmail.com>
2016-04-18 11:16:27 -0300

    applied style sheet to User

--- src/model/User.java
+++ src/model/User.java
@@ -36,89 +36,61 @@ public class User {
 		return id;
 	}
 
-
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
-
 
 	public String getName() {
 		return name;
 	}
 
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
 
 	public String getLastName() {
 		return lastName;
 	}
 
-
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-
 
 	public String getGender() {
 		return gender;
 	}
 
-
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
-
 
 	public String getPassword() {
 		return password;
 	}
 
-
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-
 
 	public String getNickname() {
 		return nickname;
 	}
 
-
-
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
 	}
-
-
+	
 	public Date getBirthDate() {
 		return birthDate;
 	}
-
 
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
