eb264421294e8014fad57a7be3da6bed87a58b5a
Keli Cristina <keliunb@gmail.com>
2016-05-02 00:39:45 -0300

    apply style sheet in  ServletDenounce.java file

--- src/control/ServletDenounce.java
+++ src/control/ServletDenounce.java
@@ -9,16 +9,13 @@ package control;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.sun.org.apache.xalan.internal.xsltc.compiler.Parser;
-
 import dao.BlogDAO;
 import dao.DenounceBlogDAO;
 import dao.DenouncePublicationDAO;
@@ -89,80 +86,80 @@ public class ServletDenounce extends HttpServlet{
 		List<Denounce> listDenounceBlog = new ArrayList<>();
 		List<Denounce> listDenouncePublication = new ArrayList<>();
 
-		try{
-			switch(action){
-			case "CreateDenounceBlog":
-				denounceBlog.setContentDenounce(request.getParameter("content"
+		try {
+			switch (action) {
+				case "CreateDenounceBlog":
+					denounceBlog.setContentDenounce(request.getParameter("content"
 																+ "Denounce"));
-				System.out.println(request.getParameter("contentDenounce"));
-				user.setId(Integer.parseInt(request.getParameter("id")));
-				int idBlog = Integer.parseInt(request.getParameter("idBlog"));
-				factoryDenounceBlogDAO.createDenounce(idBlog, denounceBlog, 
-													  user);
-				this.rd = request.getRequestDispatcher("index.jsp");
-				this.rd.forward(request, response);
-				break;
-			case "InstanceBlog":
-				String idBlogD= request.getParameter("idBlog");
-				request.setAttribute("idBlog", idBlogD);
-				this.rd = request.getRequestDispatcher("denounceBlog.jsp");
-				this.rd.forward(request, response);
-				break;
-			case "CreateDenouncePublication":
-				denouncePublication.setContentDenounce(request.getParameter(""
+					System.out.println(request.getParameter("contentDenounce"));
+					user.setId(Integer.parseInt(request.getParameter("id")));
+					int idBlog = Integer.parseInt(request.getParameter("idBlog"));
+					factoryDenounceBlogDAO.createDenounce(idBlog, denounceBlog, 
+													  	  user);
+					this.rd = request.getRequestDispatcher("index.jsp");
+					this.rd.forward(request, response);
+					break;
+				case "InstanceBlog":
+					String idBlogD= request.getParameter("idBlog");
+					request.setAttribute("idBlog", idBlogD);
+					this.rd = request.getRequestDispatcher("denounceBlog.jsp");
+					this.rd.forward(request, response);
+					break;
+				case "CreateDenouncePublication":
+					denouncePublication.setContentDenounce(request.getParameter(""
 						+ "contentDenounce"));
-				System.out.println(denouncePublication.getContentDenounce());
-				user.setId(Integer.parseInt(request.getParameter("id")));
-				int idPublication = Integer.parseInt(request.getParameter("id"
+					System.out.println(denouncePublication.getContentDenounce());
+					user.setId(Integer.parseInt(request.getParameter("id")));
+					int idPublication = Integer.parseInt(request.getParameter("id"
 						+ "Publication"));
-				factoryDenouncePublicationDAO.createDenounce(idPublication, 
+					factoryDenouncePublicationDAO.createDenounce(idPublication, 
 						denouncePublication, user);
-				this.rd = request.getRequestDispatcher("index.jsp");
-				this.rd.forward(request, response);
-				break;
-			case "InstancePublication":
-				String idPublicationD= request.getParameter("idPublication");
-				request.setAttribute("idPublication", idPublicationD);
-				this.rd = request.getRequestDispatcher("denounce"
+					this.rd = request.getRequestDispatcher("index.jsp");
+					this.rd.forward(request, response);
+					break;
+				case "InstancePublication":
+					String idPublicationD= request.getParameter("idPublication");
+					request.setAttribute("idPublication", idPublicationD);
+					this.rd = request.getRequestDispatcher("denounce"
 						+ "Publication.jsp");
-				this.rd.forward(request, response);
-				break;
-			case "ListDenounce":
-				listDenounceBlog = denounceBlogDAO.listDenounce();
-				request.setAttribute("listDenounceBlog", listDenounceBlog);
-				listDenouncePublication = denouncePublicationDAO.listDenounce();
-				request.setAttribute("listDenouncePublication", 
-									  listDenouncePublication);
-				this.rd = request.getRequestDispatcher("listDenounce.jsp");
-				this.rd.forward(request, response);
-			case "DeleteDenounceBlog":
-				String idDenounce = request.getParameter("idDenounce");
-				denounceBlogDAO.deleteDenounce(idDenounce);
-				this.rd.forward(request, response);
-				break;
-			case "DeleteDenouncePublication":
-				idDenounce = request.getParameter("idDenounce");
-				denouncePublicationDAO.deleteDenounce(idDenounce);
-				this.rd = request.getRequestDispatcher("ServletDenounce?action="
+					this.rd.forward(request, response);
+					break;
+				case "ListDenounce":
+					listDenounceBlog = denounceBlogDAO.listDenounce();
+					request.setAttribute("listDenounceBlog", listDenounceBlog);
+					listDenouncePublication = denouncePublicationDAO.listDenounce();
+					request.setAttribute("listDenouncePublication", 
+									  	  listDenouncePublication);
+					this.rd = request.getRequestDispatcher("listDenounce.jsp");
+					this.rd.forward(request, response);
+				case "DeleteDenounceBlog":
+					String idDenounce = request.getParameter("idDenounce");
+					denounceBlogDAO.deleteDenounce(idDenounce);
+					this.rd.forward(request, response);
+					break;
+				case "DeleteDenouncePublication":
+					idDenounce = request.getParameter("idDenounce");
+					denouncePublicationDAO.deleteDenounce(idDenounce);
+					this.rd = request.getRequestDispatcher("ServletDenounce?action="
 						+ "ListDenounce");
-				this.rd.forward(request, response);
-				break;
-			case "DeleteBlogDenounce":
-				System.out.println("Delete");
-				idDenounce = request.getParameter("idDenounce");
-				System.out.println(idDenounce);
-				DenounceBlogDAO denounceDAO = new DenounceBlogDAO();
-				BlogDAO blogDao = new BlogDAO();
-				Denounce denounce = denounceDAO.searchBlogDenounce(idDenounce);
-				System.out.println(denounce.getContentDenounce());
-				System.out.println("Aint"+denounce.getIdBlog());
-				blogDao.deleteBlog( String.valueOf(denounce.getIdBlog()));
-				this.rd = request.getRequestDispatcher("index.jsp");
-				this.rd.forward(request, response);
-				break;
-			default:
-				// nothing to do
-				break;
+					this.rd.forward(request, response);
+					break;
+				case "DeleteBlogDenounce":
+					System.out.println("Delete");
+					idDenounce = request.getParameter("idDenounce");
+					System.out.println(idDenounce);
+					DenounceBlogDAO denounceDAO = new DenounceBlogDAO();
+					BlogDAO blogDao = new BlogDAO();
+					Denounce denounce = denounceDAO.searchBlogDenounce(idDenounce);
+					System.out.println(denounce.getContentDenounce());
+					System.out.println("Aint"+denounce.getIdBlog());
+					blogDao.deleteBlog( String.valueOf(denounce.getIdBlog()));
+					this.rd = request.getRequestDispatcher("index.jsp");
+					this.rd.forward(request, response);
+					break;
+				default:
+					// nothing to do
+					break;
 			}
 		}
 		catch(Exception e){
