e457a919f9d539170efc91739e537766d0629d23
Keli Cristina <keliunb@gmail.com>
2016-05-02 00:12:37 -0300

    include comments of file ServletDenounce.java and fix comments of ServletDenounce class and  methods

--- src/control/ServletDenounce.java
+++ src/control/ServletDenounce.java
@@ -1,3 +1,9 @@
+/** 
+* File name: ServletDenounce.java
+* Purpose of file: this file contains the ServletDenounce class and its methods.   
+* Copyright: Copyright: This software follows GPL license.
+**/
+
 package control;
 
 import java.io.IOException;
@@ -25,140 +31,143 @@ import model.DenouncePublication;
 import model.User;
 
 @WebServlet("/ServletDenounce")
+/**
+* Class name: ServletDenounce
+* Purpose of class: This class keeps denounces. It has some specific tasks such
+* as denounce a publication, denounce a blog and delete a denounce of 
+* publication or blog.
+**/
 public class ServletDenounce extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	private RequestDispatcher rd;
-
+	/** 
+	* Method name: doGet
+	* Purpose of method: this method is used to intercept HTTP GET requests. 
+	* The HTTP GET requests are used when you get the same result everytime 
+	* you execute the request.  
+	* @param request: used to represent the HTTP request that a browser sends
+    * to the application.
+	* @param response: used to represent the HTTP response that the application
+    * sends to a browser. 
+	* @return there is no return.
+	**/
 	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+						 HttpServletResponse response) 
+						 throws ServletException, IOException {
 		assert (request != null) : "The request from user is null";
 		assert (response != null) : "The response to user is null";
 		doPost(request, response);
 	}
-
+	/** 
+	* Method name: doPost
+	* Purpose of method: this method is used to intercept HTTP POST requests. 
+	* The HTTP POST request are used when the results of the requests will 
+	* not be the same.  
+	* @param request: used to represent the HTTP request that a browser sends
+    * to the application.
+	* @param response: used to represent the HTTP response that the application
+    * sends to a browser. 
+	* @return there is no return.
+	**/
 	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+						  HttpServletResponse response) 
+						  throws ServletException, IOException {
 		assert (request != null) : "The request from user is null";
 		assert (response != null) : "The response to user is null";
-
 		String action = request.getParameter("action");
-
 		DenounceBlog denounceBlog = new DenounceBlog();
 		DenouncePublication denouncePublication = new DenouncePublication();
-		FactoryDenounceBlogDAO factoryDenounceBlogDAO = new FactoryDenounceBlogDAO();
+		FactoryDenounceBlogDAO factoryDenounceBlogDAO = new 
+				FactoryDenounceBlogDAO();
 		DenounceBlogDAO denounceBlogDAO = new DenounceBlogDAO();
-		FactoryDenouncePublicationDAO factoryDenouncePublicationDAO = new FactoryDenouncePublicationDAO();
-		DenouncePublicationDAO denouncePublicationDAO = new DenouncePublicationDAO();
+		FactoryDenouncePublicationDAO factoryDenouncePublicationDAO = new 
+				FactoryDenouncePublicationDAO();
+		DenouncePublicationDAO denouncePublicationDAO = new 
+				DenouncePublicationDAO();
 		User user = new User();
 		Blog blog = new Blog();
 		List<Denounce> listDenounceBlog = new ArrayList<>();
 		List<Denounce> listDenouncePublication = new ArrayList<>();
 
-
 		try{
 			switch(action){
 			case "CreateDenounceBlog":
-				denounceBlog.setContentDenounce(request.getParameter("contentDenounce"));
+				denounceBlog.setContentDenounce(request.getParameter("content"
+																+ "Denounce"));
 				System.out.println(request.getParameter("contentDenounce"));
 				user.setId(Integer.parseInt(request.getParameter("id")));
 				int idBlog = Integer.parseInt(request.getParameter("idBlog"));
-				factoryDenounceBlogDAO.createDenounce(idBlog, denounceBlog, user);
-
+				factoryDenounceBlogDAO.createDenounce(idBlog, denounceBlog, 
+													  user);
 				this.rd = request.getRequestDispatcher("index.jsp");
 				this.rd.forward(request, response);
 				break;
-
-
 			case "InstanceBlog":
 				String idBlogD= request.getParameter("idBlog");
 				request.setAttribute("idBlog", idBlogD);
 				this.rd = request.getRequestDispatcher("denounceBlog.jsp");
 				this.rd.forward(request, response);
 				break;
-
-
 			case "CreateDenouncePublication":
-				denouncePublication.setContentDenounce(request.getParameter("contentDenounce"));
+				denouncePublication.setContentDenounce(request.getParameter(""
+						+ "contentDenounce"));
 				System.out.println(denouncePublication.getContentDenounce());
 				user.setId(Integer.parseInt(request.getParameter("id")));
-				int idPublication = Integer.parseInt(request.getParameter("idPublication"));
-				factoryDenouncePublicationDAO.createDenounce(idPublication, denouncePublication, user);
-
+				int idPublication = Integer.parseInt(request.getParameter("id"
+						+ "Publication"));
+				factoryDenouncePublicationDAO.createDenounce(idPublication, 
+						denouncePublication, user);
 				this.rd = request.getRequestDispatcher("index.jsp");
 				this.rd.forward(request, response);
 				break;
-
 			case "InstancePublication":
 				String idPublicationD= request.getParameter("idPublication");
 				request.setAttribute("idPublication", idPublicationD);
-				this.rd = request.getRequestDispatcher("denouncePublication.jsp");
+				this.rd = request.getRequestDispatcher("denounce"
+						+ "Publication.jsp");
 				this.rd.forward(request, response);
 				break;
-
-
 			case "ListDenounce":
 				listDenounceBlog = denounceBlogDAO.listDenounce();
 				request.setAttribute("listDenounceBlog", listDenounceBlog);
-
 				listDenouncePublication = denouncePublicationDAO.listDenounce();
-				request.setAttribute("listDenouncePublication", listDenouncePublication);
-
+				request.setAttribute("listDenouncePublication", 
+									  listDenouncePublication);
 				this.rd = request.getRequestDispatcher("listDenounce.jsp");
 				this.rd.forward(request, response);
-
 			case "DeleteDenounceBlog":
 				String idDenounce = request.getParameter("idDenounce");
 				denounceBlogDAO.deleteDenounce(idDenounce);
-
-
-
 				this.rd.forward(request, response);
-
-			break;
-
+				break;
 			case "DeleteDenouncePublication":
-
 				idDenounce = request.getParameter("idDenounce");
-
-
 				denouncePublicationDAO.deleteDenounce(idDenounce);
-				this.rd = request.getRequestDispatcher("ServletDenounce?action=ListDenounce");
+				this.rd = request.getRequestDispatcher("ServletDenounce?action="
+						+ "ListDenounce");
 				this.rd.forward(request, response);
-
-			break;
-
+				break;
 			case "DeleteBlogDenounce":
 				System.out.println("Delete");
 				idDenounce = request.getParameter("idDenounce");
 				System.out.println(idDenounce);
 				DenounceBlogDAO denounceDAO = new DenounceBlogDAO();
-
-					BlogDAO blogDao = new BlogDAO();
-
-
+				BlogDAO blogDao = new BlogDAO();
 				Denounce denounce = denounceDAO.searchBlogDenounce(idDenounce);
-
-				//System.out.println(denuncia.getIdBlog());
 				System.out.println(denounce.getContentDenounce());
-
-
 				System.out.println("Aint"+denounce.getIdBlog());
-
-
-
 				blogDao.deleteBlog( String.valueOf(denounce.getIdBlog()));
-
 				this.rd = request.getRequestDispatcher("index.jsp");
 				this.rd.forward(request, response);
-
-			break;
+				break;
+			default:
+				// nothing to do
+				break;
+			}
 		}
-
-	}
 		catch(Exception e){
 			// TODO: handle exception)
 		}
-	  }
-
 	}
+}
 
