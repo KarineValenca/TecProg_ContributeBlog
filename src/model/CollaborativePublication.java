45d31b404380953232b89edfe01e07900271c1bb
Keli Cristina <keliunb@gmail.com>
2016-05-04 00:48:06 -0300

    insert comment CollaborativePublication.java file

--- src/model/CollaborativePublication.java
+++ src/model/CollaborativePublication.java
@@ -1,27 +1,25 @@
+/** 
+* File name: CollaborativePublication.java
+* Purpose of file: this file contains the class CollaborativePublication and its methods.   
+* Copyright: This software follows GPL license.
+**/
+
 package model;
 
 public class CollaborativePublication extends Publication {
 
-
 	boolean statusPublication;
 	User user;
-
 
 	public CollaborativePublication() {
 		// TODO Auto-generated constructor stub
 	}
 
-
 	public CollaborativePublication(int idPublication, String titlePublication,
 			String categoryPublication, String contentPublication, Blog idBlog,
 			int gradePublication) {
-		super(idPublication, titlePublication, categoryPublication, contentPublication,
-				idBlog, gradePublication);
+		super(idPublication, titlePublication, categoryPublication,
+			  contentPublication,	idBlog, gradePublication);
 		// TODO Auto-generated constructor stub
 	}
-
-
-
-
-
 }
