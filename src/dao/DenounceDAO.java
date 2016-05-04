56be7197e8de4e867bfadf998482f7b4373e6fe2
Keli Cristina <keliunb@gmail.com>
2016-05-04 00:37:44 -0300

    insert comments of file CollaborationDAO.java and DenounceDAO interface and  methods

--- src/dao/DenounceDAO.java
+++ src/dao/DenounceDAO.java
@@ -1,10 +1,35 @@
+/** 
+* File name: DenounceDAO.java
+* Purpose of file: this file contains the interface DenounceDAO and its methods.   
+* Copyright: This software follows GPL license.
+**/
+
 package dao;
 
 import java.util.List;
 
 import model.Denounce;
 
+/**
+* Interface name: DenounceDAO
+* Purpose of interface: this interface that defines the methods: listDenounce
+* and deleteDenounce. 
+**/
 public interface DenounceDAO {
+	
+	/** 
+	* Method name: listDenounce
+	* Purpose of method: setting method listarDenuncia.  
+	* @param: there is no param.
+	* @return there is no return.
+	**/
 	public List<Denounce> listDenounce ();
+	
+	/** 
+	* Method name: deleteDenounce
+	* Purpose of method: setting method listarDenuncia.  
+	* @param id denounce: unique identify of denounce to be deleted.
+	* @return there is no return.
+	**/
 	public void deleteDenounce(String idDenounce);
 }
