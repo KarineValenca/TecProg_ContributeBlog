7c033b7ac5bd922d9e2c0ccaebdbfbe3ba58bdb3
Keli Cristina <keliunb@gmail.com>
2016-05-05 15:57:17 -0300

    insert comments in DenounceBlog.java file

--- src/model/DenounceBlog.java
+++ src/model/DenounceBlog.java
@@ -1,3 +1,9 @@
+/** 
+* File name: DenounceBlog.java
+* Purpose of file: this file contains the DenounceBlog class and methods.   
+* Copyright: This software follows GPL license.
+**/
+
 package model;
 
 import java.util.Date;
