93f114aafeaea78ffe01e4146b279b2f6ac6c796
Keli Cristina <keliunb@gmail.com>
2016-05-05 15:48:56 -0300

    insert comments in Denounce.java file

--- src/model/Denounce.java
+++ src/model/Denounce.java
@@ -1,18 +1,29 @@
+/** 
+* File name: Denounce.java
+* Purpose of file: this file contains the Denounce class, attributes and its methods.   
+* Copyright: This software follows GPL license.
+**/
+
 package model;
 
 import java.util.Date;
 
+
+/**
+* Class name: Denounce
+* Purpose of class: This class is used to store all attributes from a denounce.
+**/
 public class Denounce {
 	private int idDenounce;
 	private Date dateDenounce;
 	private String contentDenounce;
 	private int idBlog; 
 	
-	public Denounce(){
-		
+	public Denounce(){		
 	}
 	
-	public Denounce(int idDenounce, Date dateDenounce, String contentDenounce, int idBlog){
+	public Denounce(int idDenounce, Date dateDenounce, String contentDenounce, 
+					int idBlog){
 		this.idDenounce = idDenounce;
 		this.dateDenounce = dateDenounce;
 		this.contentDenounce = contentDenounce;
@@ -23,6 +34,7 @@ public class Denounce {
 	public int getIdDenounce() {
 		return idDenounce;
 	}
+	
 	public int getIdBlog() {
 		return idBlog;
 	}
@@ -34,17 +46,20 @@ public class Denounce {
 	public void setIdDenounce(int idDenounce) {
 		this.idDenounce = idDenounce;
 	}
+	
 	public Date getDateDenounce() {
 		return dateDenounce;
 	}
+	
 	public void setDateDenounce(Date dateDenounce) {
 		this.dateDenounce = dateDenounce;
 	}
+	
 	public String getContentDenounce() {
 		return contentDenounce;
 	}
+	
 	public void setContentDenounce(String contentDenounce) {
 		this.contentDenounce = contentDenounce;
 	}
-	
 }
