da467a7e33f4346623fcba8cc764a1485f4cb96e
Keli Cristina <keliunb@gmail.com>
2016-05-05 09:23:44 -0300

    insert comment DenouncePublicationDAO.java file, class and methods

--- src/dao/DenouncePublicationDAO.java
+++ src/dao/DenouncePublicationDAO.java
@@ -1,3 +1,9 @@
+/** 
+* File name: DenouncePublicarionDAO.java
+* Purpose of file: this file contains the DenouncePublicationDAO and its methods.   
+* Copyright: This software follows GPL license.
+**/
+
 package dao;
 
 import java.sql.Connection;
@@ -6,12 +12,23 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
 import model.Denounce;
 import model.DenouncePublication;
 
-
+/**
+* Class name: DenouncePublicationDAO
+* Purpose of class: This class has the methods used to connect to the database 
+* and execute methods to list and make changes related to denounces. It is used
+* by other classes of data persistence.
+**/
 public class DenouncePublicationDAO extends ConnectionFactory implements DenounceDAO{
+
+	/** 
+	* Method name: listDenounce
+	* Purpose of method: this method is responsible for listing the denounces.  
+	* @param: there is no param.
+	* @return list: array list of denounces blog.
+	**/	
 	public List<Denounce> listDenounce() {
 		List<Denounce> list = new ArrayList<>();
 		try {
@@ -32,7 +49,13 @@ public class DenouncePublicationDAO extends ConnectionFactory implements Denounc
 		}
 		return list;
 	}
-	
+
+	/** 
+	* Method name: deleteDenounce
+	* Purpose of method: This method is responsible for making the exclusion of a denounce database.  
+	* @param idDenounce: unique identify of denounce to be deleted.
+	* @return: there is no return.
+	**/	
 	public void deleteDenounce(String idDenounce) {
 		assert(idDenounce != null ) : "Unexpected error: the denounce identifier is receiving null";
 		try {
