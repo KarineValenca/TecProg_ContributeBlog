f2733d8fae66f9d4d863dfaff8c7b8c7d50d58c3
Murilo Duarte <muriloduartegoncalves@hotmail.com>
2016-05-05 13:19:13 -0300

    insert comment PublicationGeneral DAO file

--- src/dao/PublicationGeneral.java
+++ src/dao/PublicationGeneral.java
@@ -1,6 +1,6 @@
 /*
  * Class name: PublicationGeneral.java
- * Purpose of class: This file corresponds to persistence class Publication.
+ * Purpose of file: This file contains the PublicationGeneral DAO class and its methods.
  * Copyright: This software follows GPL license.
  */
 
@@ -8,9 +8,27 @@ package dao;
 
 import model.Publication;
 
+/**
+ * Class name: PublicationGeneral
+ * Purpose of class: This class is used to createPublication and listPublication, about Generals Publications.
+ * This class that implements interface standard.
+ **/
 public interface PublicationGeneral {
+            /**
+             * Method name: createPublication
+             * Purpose of method: Represents the association of a text publication with a blog.
+             * @param idBlog: used to identify the blog.
+             * @param publicacao: The text of the publication that will be associated with the blog
+             * @param idPublication: the text identifier in the system
+             **/
+	public void createPublication(int idBlog, Publication publication);
 
-	public void createPublication(int idBlog, Publication publicacao);
+            /**
+             * Method name: listPublication
+             * Purpose of method: Matches the publication of Data Display you want to input the
+             * identification attribute it.
+             * @param idPublication: used to identify the publication.
+             **/
 	public Publication  listPublication(String idPublication);
 
 }
