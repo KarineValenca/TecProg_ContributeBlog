bcb0e7830baa8c47c8536f432648fb5777739c8c
Murilo Duarte <muriloduartegoncalves@hotmail.com>
2016-05-05 23:18:00 -0300

    insert comment ServletPublication SERVLET file

--- src/control/ServletPublication.java
+++ src/control/ServletPublication.java
@@ -1,7 +1,6 @@
 /*
- * Class name: ServletPublication.java
- * Purpose of class: This class is used to the methods of control Publication,
- * used to the methods create,list, include, delete,rate, edit and show list comments.
+ * Class file: ServletPublication.java
+ * Purpose of class: This file contains the ServletPublication class and its methods.
  * Copyright: This software follows GPL license.
  */
 
@@ -25,19 +24,41 @@ import model.Blog;
 import model.Comment;
 import model.Publication;
 
+/**
+ * Class name: ServletPublication
+ * Purpose of class: This used to the methods create, list, include, delete, rate, edit and
+ * show list comments.
+ **/
 @WebServlet("/ServletPublication")
 public class ServletPublication extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
 	private RequestDispatcher rd;
 
+	/**
+	 * Method name: doGet
+	 * Purpose of method: This method is used to intercept HTTP GET requests.
+	 * The HTTP GET requests are used when you get the same result everytime.
+	 * @param request: used to represent the HTTP request that a browser sends.
+	 * @param response: used to represent the HTTP response that the application.
+	 * @return: there is no return
+	 **/
 	protected void doGet(HttpServletRequest request, HttpServletResponse
-		                                                               response) throws ServletException, IOException {
+		                       response) throws ServletException, IOException {
 		doPost(request, response);
 	}
 
+	/**
+	 * Method name: doPost
+	 * Purpose of method: This method is used to intercept HTTP POST requests.
+ 	 * The HTTP POST request are used when the results of the requests will
+  	 * not be the same.
+	 * @param request: used to represent the HTTP request that a browser sends.
+	 * @param response: used to represent the HTTP response that the application.
+	 * @return: there is no return
+	 **/
 	protected void doPost(HttpServletRequest request, HttpServletResponse
-		                                                               response) throws ServletException, IOException {
+		                        response) throws ServletException, IOException {
 		String action = request.getParameter("action");
 		Publication publication = new Publication();
 		PublicationDAO publicationDAO = new PublicationDAO();
@@ -47,6 +68,7 @@ public class ServletPublication extends HttpServlet{
 
 		try{
 			switch(action){
+			// this case is used for the User to create a publication in a blog
 			case "CreatePublication":
 				int idBlogInsert = ( Integer.parseInt(request.getParameter("idBlog")));
 
@@ -58,6 +80,8 @@ public class ServletPublication extends HttpServlet{
 				this.rd = request.getRequestDispatcher("index.jsp");
 				this.rd.forward(request, response);
 				break;
+
+			// this case is used to create an object of publication and instantiates it
 			case "PublicationInstance":
 				String idBlog = request.getParameter("idBlog");
 
@@ -66,6 +90,8 @@ public class ServletPublication extends HttpServlet{
 				this.rd = request.getRequestDispatcher("createPublication.jsp");
 				this.rd.forward(request, response);
 				break;
+
+			// this case is used to allow the user update data in their publication
 			case "EditPublication":
 				String idPublication = request.getParameter("idPublication");
 
@@ -77,6 +103,8 @@ public class ServletPublication extends HttpServlet{
 				this.rd = request.getRequestDispatcher("index.jsp");
 				this.rd.forward(request, response);
 				break;
+
+			// this case is used to show the all publications
 			case "ListPublication":
 				idPublication = request.getParameter("idPublication");
 				publication = publicationDAO.listPublication(idPublication);
@@ -86,6 +114,8 @@ public class ServletPublication extends HttpServlet{
 				this.rd = request.getRequestDispatcher("editPublication.jsp");
 				this.rd.forward(request, response);
 				break;
+
+			// this case is used to allow the user delete their publication
 			case "DeletePublication":
 				idPublication = request.getParameter("idPublication");
 				publicationDAO.deletePublication(idPublication);
@@ -93,6 +123,8 @@ public class ServletPublication extends HttpServlet{
 				this.rd = request.getRequestDispatcher("index.jsp");
 				this.rd.forward(request, response);
 				break;
+
+			// this case is used for a User to rate a publication
 			case "RatePublication":
 				String gradePublication = request.getParameter("gradePublication");
 				int currentRate = publication.getGradePublication();
@@ -105,6 +137,8 @@ public class ServletPublication extends HttpServlet{
 				this.rd = request.getRequestDispatcher("index.jsp");
 				this.rd.forward(request, response);
 				break;
+
+			// this case is used to show the all comments about publication
 			case "ListComments":
 				idPublication = request.getParameter("idPublication");
 				listComments = publicationDAO.listComents(idPublication);
@@ -115,8 +149,6 @@ public class ServletPublication extends HttpServlet{
 				this.rd.forward(request, response);
 				break;
 			}
-
-
 		}
 		catch(Exception e){
 			// TODO: handle exception
