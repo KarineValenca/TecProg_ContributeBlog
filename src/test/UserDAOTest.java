edf123b9b5159b41fb98f2218d9741473c1994d7
Karine Valença <valenca.karine@gmail.com>
2016-05-22 14:29:53 -0300

    success test to CreateUser

--- src/test/UserDAOTest.java
+++ src/test/UserDAOTest.java
@@ -2,13 +2,40 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Test;
+
+import dao.UserDAO;
+import model.User;
 
 public class UserDAOTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreateUser() throws ParseException {
+		User user = new User();
+		user.setName("test");
+		user.setLastName("test");
+		user.setEmail("test@test.com");
+		user.setGender("Feminino");
+		user.setPassword("test");
+		user.setNickname("test");
+		
+		String date = "2014-10-03";
+		DateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
+		Date birthDate = formatDate.parse(date);
+		user.setBirthDate(birthDate);
+		
+		UserDAO userDAO = new UserDAO();
+		boolean wasCreated = false;
+		
+		wasCreated = userDAO.createUser(user);
+		
+		assertTrue(wasCreated);
+		
 	}
 
 }
