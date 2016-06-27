2b8b05e85dddcd55d5b460ec58b867dc97ee6345
Keli Cristina Borges <keliunb@gmail.com>
2016-06-26 22:18:01 -0700

    insert test for FactoryDenouncePublicationDAOTest and fix FactoryDenounceBlogDAOTest class

--- src/test/FactoryDenouncePublicationDAOTest.java
+++ src/test/FactoryDenouncePublicationDAOTest.java
@@ -11,13 +11,13 @@ import dao.FactoryDenounceBlogDAO;
 import model.Denounce;
 import model.User;
 
-public class FactoryDenounceBlogDAOTest {
+public class FactoryDenouncePublicationDAOTest {
 
 	@Test
 	public void testCreateDenounce() throws ParseException {
 		
 		Date now = new Date(0);
-		int idBlog = 1;
+		int idPublication = 1;
 		Denounce denounce = new Denounce();
 		denounce.idDenounce = 1;
 		denounce.dateDenounce = now;
@@ -26,13 +26,13 @@ public class FactoryDenounceBlogDAOTest {
 		User user = new User();
 		user.id = 1;		
 		FactoryDenounceBlogDAO factoryDenounceBlogDAO = new FactoryDenounceBlogDAO();
-		assertTrue(factoryDenounceBlogDAO.createDenounce(idBlog, denounce, user));
+		assertTrue(factoryDenounceBlogDAO.createDenounce(idPublication, denounce, user));
 	}
 	
 	@Test
 	public void testFailCreateDenounce() {
 		Date now = new Date(0);
-		int idBlog = 1;
+		int idPublication = 1;
 		Denounce denounce = new Denounce();
 		denounce.idDenounce = 1;
 		denounce.dateDenounce = now;
@@ -41,7 +41,7 @@ public class FactoryDenounceBlogDAOTest {
 		User user = new User();
 		user.id = 1;		
 		FactoryDenounceBlogDAO factoryDenounceBlogDAO = new FactoryDenounceBlogDAO();
-		assertFalse(factoryDenounceBlogDAO.createDenounce(idBlog, denounce,null));
+		assertFalse(factoryDenounceBlogDAO.createDenounce(idPublication, denounce,null));
 	}
 	
 }
